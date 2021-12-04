e0514b8585c7cee48bb0ea8cd67079e69c04ff7f
Geomale25 <male2.dr@gmail.com>
2021-12-04 03:48:28 +0200

    Number buttons and backspace button are now functional.

--- app/src/main/java/com/example/myfirstcalculatorapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstcalculatorapp/MainActivity.java
@@ -7,12 +7,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText display;
     private Button zero, one, two, three, four, five, six, seven, eight, nine, clear, add, subtract, multiply, divide, plusMinus, point, parenthesis;
     private ImageButton backspace;
+    private TextView solution;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
 
         backspace = findViewById(R.id.backspaceBTN);
 
+        solution = findViewById(R.id.solution);
+
         display.setShowSoftInputOnFocus(false);
         display.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,6 +59,121 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        zero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateText("0");
+            }
+        });
+
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateText("1");
+            }
+        });
+
+        two.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateText("2");
+            }
+        });
+
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateText("3");
+            }
+        });
+
+        four.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateText("4");
+            }
+        });
+
+        five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateText("5");
+            }
+        });
+
+        six.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateText("6");
+            }
+        });
+
+        seven.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateText("7");
+            }
+        });
+
+        eight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateText("8");
+            }
+        });
+
+        nine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateText("9");
+            }
+        });
+
+        backspace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateText("delete");
+            }
+        });
+
+        parenthesis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateText("delete");
+            }
+        });
+
+    }
+
+    private void updateText(String strToAdd){
+        String oldStr = display.getText().toString();
+
+        int cursorPos = display.getSelectionStart();
+        String leftStr = oldStr.substring(0, cursorPos);
+        String rightStr = oldStr.substring(cursorPos);
+        System.out.println("\n================\nLeft: " + leftStr + "\nRight: " + rightStr + "\n================");
+
+        System.out.println(">>>>>>>>>>>>>>>>>> " + display.getText().toString());
+
+        if(strToAdd != "delete"){
+            String newStr = String.format("%s%s%s", leftStr, strToAdd, rightStr);
+            display.setText(newStr);
+            display.setSelection(cursorPos + 1);
+        }else{
+            if(cursorPos != 0){
+                System.out.println("DELETING...");
+                leftStr = oldStr.substring(0, cursorPos-1);
+                String newStr = String.format("%s%s", leftStr, rightStr);
+                display.setText(newStr);
+                display.setSelection(cursorPos - 1);
+            }
+
+        }
+
+    }
+
+    private void clearText(){
+        display.setText("");
     }
 
 }
